bfe1e35d1d35f3081fd670daa3570ceca6cae4b3
Connor Francis <connorfrancis@live.com>
2026-02-01 15:25:07 -0700

    works just need context

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,7 +2,8 @@ import { useState } from "react"
 import '../index.css'
 
 
-export default function Game(){
+
+export default function Game() {
 
 
     let [score, setScore] = useState(0)
@@ -12,52 +13,61 @@ export default function Game(){
 
 
 
-    function generateMoleIndexNumber(){
-        setMoleIndex(moleIndex = Math.floor(Math.random()* 10))
+    function generateMoleIndexNumber() {
+        setMoleIndex(moleIndex = Math.floor(Math.random() * 9) + 1)
+        console.log(moleIndex)
     }
 
-    function removeMoleIndexNumber(){
-        setMoleIndex(moleIndex=null)
+    function removeMoleIndexNumber() {
+        setMoleIndex(moleIndex = null)
     }
 
 
 
-    function addScore(){
-        setScore(score+=1)
+    function addScore() {
+        setScore(score += 1)
     }
 
     function moleDisplayFunction(i) {
         if (i === moleIndex) {
-          return (
-            <div key={i} onClick={() => { addScore(); removeMoleIndexNumber(); generateMoleIndexNumber(); }}>
-              <img src="/mole.png" alt="Mole" />
-            </div>
-          );
+            return (
+                <div key={i} onClick={() => { addScore(); removeMoleIndexNumber(); generateMoleIndexNumber(); }}>
+                    <img src="/mole.png" alt="Mole" />
+                </div>
+            );
         }
         return (
-          <div key={i}>
-            <img src="/hole.png" alt="Hole" />
-          </div>
+            <div key={i}>
+                <img src="/hole.png" alt="Hole" />
+            </div>
         );
-      }
+    }
 
 
 
-    return(
+    return (
         <>
-        <h1>Time to play!</h1>
-        <p>Time remaining: </p>
-        <p>{score}</p>
-        {[1,2,3,4,5,6,7,8,9].map((i)=>{
-            return moleDisplayFunction(i)
-        })}   
-
-        
-        
+            <h1>Time to play!</h1>
+            <h4>Score: {score}</h4>
+            <button onClick={() => {
+                location.reload()
+            }}>Restart</button>
 
 
 
-    </>
-    
-)
+            <div className="board">
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => {
+                    return moleDisplayFunction(i)
+                })}
+            </div>
+
+
+
+
+
+
+
+        </>
+
+    )
 }
