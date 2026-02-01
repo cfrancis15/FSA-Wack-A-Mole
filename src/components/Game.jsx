00cf3d1075a815173649e06ea56cc301ee120b30
Connor Francis <connorfrancis@live.com>
2026-02-01 14:01:38 -0700

    game works, need to add time now

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,21 +1,62 @@
 import { useState } from "react"
-
+import '../index.css'
 
 
 export default function Game(){
 
-    
+
     let [score, setScore] = useState(0)
+
+    //hole states
+    let [moleIndex, setMoleIndex] = useState(1)
+
+
+
+    function generateMoleIndexNumber(){
+        setMoleIndex(moleIndex = Math.floor(Math.random()* 10))
+    }
+
+    function removeMoleIndexNumber(){
+        setMoleIndex(moleIndex=null)
+    }
+
+
 
     function addScore(){
         setScore(score+=1)
     }
 
+    function moleDisplayFunction(i) {
+        if (i === moleIndex) {
+          return (
+            <div key={i} onClick={() => { addScore(); removeMoleIndexNumber(); generateMoleIndexNumber(); }}>
+              <img src="/mole.png" alt="Mole" />
+            </div>
+          );
+        }
+        return (
+          <div key={i}>
+            <img src="/hole.png" alt="Hole" />
+          </div>
+        );
+      }
+
+
+
     return(
         <>
-        <h1>Game goes here</h1>
-        <button onClick={()=>addScore()}>Add 1</button>
+        <h1>Time to play!</h1>
+        <p>Time remaining: </p>
         <p>{score}</p>
+        {[1,2,3,4,5,6,7,8,9].map((i)=>{
+            return moleDisplayFunction(i)
+        })}   
+
+        
+        
+
+
+
     </>
     
 )
